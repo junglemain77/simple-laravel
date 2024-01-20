fb698ed92c0f05a9a5bfe11a047f634aef0749a0
Val <valenios44@gmail.com>
2024-01-20 20:27:11 +0700

    Tutorial

--- belajarlaravel/routes/web.php
+++ belajarlaravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Vale",
+        "email" => "tutorvale@vale.com",
+        "image" => "img/iso.jpg",
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

5abed3cb91d399da492f735fdf4afeeee706dc8e
Val <valenios44@gmail.com>
2024-01-25 22:35:48 +0700

    Minor Change

--- belajarlaravel/resources/views/about.blade.php
+++ belajarlaravel/resources/views/about.blade.php
@@ -4,5 +4,5 @@
     <h1>About</h1>
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="{{ $image }}" alt="{{ $name }}" width="300">
+    <img src="{{ $image }}" alt="{{ $name }}" width="300" class="img-thumbnail rounded-circle">
 @endsection

57a676ad9f58eeb922cb32de43a912ad93f8231d
Val <valenios44@gmail.com>
2024-02-08 23:41:21 +0700

    Redesign UI part 1

--- belajarlaravel/resources/views/post.blade.php
+++ belajarlaravel/resources/views/post.blade.php
@@ -3,7 +3,8 @@
 @section('container')
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
-    <h5>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug}}" class="text-decoration-none">{{ $post->category->name }}</h5>
+    <h5>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in 
+    <a href="/categories/{{ $post->category->slug}}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
     {!! $post->body !!}
 </article>
 
